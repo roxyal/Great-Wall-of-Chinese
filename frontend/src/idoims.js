6a941dc6cbd21737088748a7e6a1f72a20df68e5
AsherFYD <AsherFYD@gmail.com>
2022-10-14 18:17:04 +0800

    using phaser from cdn instead

---
+++ frontend/src/idoims.js
@@ -0,0 +1,25 @@
+import {IdiomsWorld} from "./scenes/IdiomsWorld.js";
+
+let config = {
+    width: 800,
+    height: 600,
+    parent: 'phaser-example',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false
+        }
+    },
+    scale: {
+        // Fit to window
+        mode: Phaser.Scale.FIT,
+        // Center vertically and horizontally
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    dom: {
+        createContainer: true
+    },
+    scene: [IdiomsWorld]
+}
+
+let game = new Phaser.Game(config);
