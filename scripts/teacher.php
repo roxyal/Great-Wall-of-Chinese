2cf12cba98809c7d3e080fa1b0ee02b8193cb7e2
zappe <zapper5312@hotmail.com>
2022-10-19 16:51:57 +0800

    clean student/teacher code
    Included the checkAccountIdExists for the respesctive function

--- scripts/teacher.php
+++ scripts/teacher.php
@@ -1,5 +1,6 @@
 <?php
 
+//require "functions_utility.php";
 // A Teacher class that holds all the function needed for teacher
 class Teacher{
     
@@ -12,7 +13,7 @@ class Teacher{
     }
     
     // A utility function to check if the teacher has any students
-    public function checkTeacherHasStudentExists($teacher_account_id)
+    public function checkTeacherHasStudentExists($teacher_account_id) : bool
     {
         $sql = "SELECT * FROM students WHERE teacher_account_id = ?";
         $stmt = $this->conn->prepare($sql);
@@ -29,8 +30,11 @@ class Teacher{
     // A function for teachers to view students' summary report
     public function viewSummaryReport($teacher_account_id)
     {
+        // Check to see if account_id exist
+        if (!checkTeacherExists($teacher_account_id)) return 1;
+
         // Check to see if teacher has students
-        if (!$this->checkTeacherHasStudentExists($teacher_account_id)) return 1;
+        if (!$this->checkTeacherHasStudentExists($teacher_account_id)) return 2;
         
         $sql = "SELECT student_id, character_type, idiom_lower_accuracy, idiom_upper_accuracy, fill_lower_accuracy, fill_upper_accuracy, pinyin_lower_accuracy, pinyin_upper_accuracy FROM students WHERE teacher_account_id = ?";
         $stmt = $this->conn->prepare($sql);
@@ -50,7 +54,7 @@ class Teacher{
         else
         {
             if($debug_mode) echo $this->conn->error;
-                return 2; // ERROR with database SQL
+                return 3; // ERROR with database SQL
         }       
     }
 }
