834e6eb9f2c12375404f0c0ac2125c6ae467cb55
Skythel <14922694+Skythel@users.noreply.github.com>
2022-10-26 00:55:47 +0800

    added other characters on world map

--- websocket/socket.js
+++ websocket/socket.js
@@ -23,20 +23,29 @@ function generateSocketAuth() {
 //     }
 // });
 function loadPlayers(players) {
-    console.log("loading players");
+    // console.log("loading players");
     console.log(players);
     for(let player in players) {
-        let matches = players[player].match(/^(\d+)-(\d+)-(\d+)$/);
+        let matches = players[player].match(/^(\d)-(.+)-(.+)$/);
         let characterType = matches[1];
         let posX = parseInt(matches[2]);
         let posY = parseInt(matches[3]);
         spawnPlayer(player, characterType, posX, posY);
     }
 }
-import { spawnPlayer } from "../frontend/src/idioms.js";
+import { spawnPlayer, movePlayer, destroyPlayer } from "../frontend/src/idioms.js";
 // export var socket;
 var token;
 var world;
+var updateLoop;
+var moves = {};
+// // Set an interval to check the movement queue
+// var moveQueue = window.setInterval(function() {
+//     // If movement queue has any items
+//     for(let [key, value] of Object.entries(moves)) {
+//         movePlayer(key, value[0], value[1], value[2]);
+//     }
+// }, 1000);
 generateSocketAuth().then(result => {
     token = result;
     if(window.location.pathname.includes("idioms")) world = "Idioms";
@@ -46,6 +55,12 @@ generateSocketAuth().then(result => {
     
     // Create a new WebSocket.
     socket = window.location.hostname == "localhost" ? new WebSocket(`ws://${window.location.hostname}:8888?token=${token}&world=${world}`) : new WebSocket(`wss://${window.location.hostname}/wss2/:8888?token=${token}&world=${world}`);
+
+    // Send an update request to the socket every few seconds to account for packet loss
+    clearInterval(updateLoop);
+    updateLoop = window.setInterval(function() {
+        socket.send("/update");
+    }, 5000); 
 
     function transmitMessage() {
         socket.send( message.value );
@@ -57,14 +72,52 @@ generateSocketAuth().then(result => {
         // Spawn the players that are already logged in
         try {
             let players = JSON.parse(e.data);
+
+            // Do the initial load to fetch already logged in players
+            if(players.hasOwnProperty("firstload") && players["firstload"] == 0) {
+                console.log("initial loading...");
+                window.setTimeout(function() {
+                    loadPlayers(players);
+                }, 3000); 
+                delete players["firstload"];
+            }
             console.log(players);
-            // Setting it to 3s to wait before loading players, can try changing to on phaser load
-            window.setTimeout(function() {
-                loadPlayers(players);
-            }, 3000); 
+
+            try {
+                let playersToUpdate = {};
+
+                // Check if the players' latest positions are the same as their last recorded position in moves
+                for(let [key, value] of Object.entries(players)) {
+
+                    // Save the player's current location if their name did not exist in moves
+                    if(!moves.hasOwnProperty(key)) {
+                        moves[key] = [value.split("-")];
+                        continue;
+                    }
+
+                    // Check if the player's last move location is equal to their current location
+                    let player = moves[key];
+                    let concat = `${player[player.length-1][0]}-${player[player.length-1][1]}-${player[player.length-1][2]}`;
+                    console.log(key, value, concat);
+
+                    // Save the player's last updated location
+                    moves[key].push(concat.split("-"));
+
+                    if(value == concat) {
+                        console.log("skipping "+key);
+                        continue;
+                    }
+                    playersToUpdate[key] = players[key];
+                }
+
+                if(Object.entries(playersToUpdate).length > 0) 
+                    loadPlayers(playersToUpdate);
+            } catch (e2) {
+                // console.log(e2);
+            }
         } catch (e) {
-            console.log("spawnPlayer failed");
-            console.log(e);
+            // console.log("spawnPlayer failed");
+            // console.log(e);
         }
 
         // message will come in the format:
@@ -94,14 +147,34 @@ generateSocketAuth().then(result => {
             }
             else if(type == "connect") {
                 // [connect] username: characterType
-                spawnPlayer(matches[2], matches[3], 200, 400);
+                spawnPlayer(sender, message, 200, 400);
             }
             else if(type == "disconnect") {
-                // spawnPlayer(matches[1], matches[2], matches[3]);
+                destroyPlayer(sender);
+                if(moves.hasOwnProperty(sender)) delete moves[sender];
             }
             else if(type == "move") {
-                // [move] username: x200 y400
-                // movePlayer()
+                // [move] username: c1 x200 y400 t0
+                // Receive a move event meaning at least one player's position has changed. Create a queue of "moves" for each user. 
+                let coords = message.match(/^c(\d) x(.+) y(.+) t(.+)$/);
+                if(!moves.hasOwnProperty(sender)) {
+                    moves[sender] = [];
+                }
+                else {
+                    // To make smoother animations, find the number of Phaser updates() that have passed since last location data
+                    let dt = coords[4]-moves[sender][moves[sender].length-1][3];
+                    // Only send location data newer than x updates from the previous data
+                    if(dt > 10) {
+                        // To prevent sending every single movement to Phaser, wait until there are >2 movements in the same player's queue
+                        // if(moves[sender].length > 2) {
+                            movePlayer(sender, coords[1], coords[2], coords[3], dt);
+                            moves[sender] = [[coords[1], coords[2], coords[3], coords[4]]];
+                        // }
+                    }
+                }
+                // Add the new location data to moves
+                moves[sender].push([coords[1], coords[2], coords[3], coords[4]]);
+                console.log(moves);
             }
             else if(/^to (.+)$/.test(type)) {
                 // private message sent from the client
--- websocket/src/Socket.php
+++ websocket/src/Socket.php
@@ -60,7 +60,7 @@ class Socket implements MessageComponentInterface {
                 $client->pvpStatus = ["Available", ""]; 
 
                 // Pass the coordinates and character id to all logged in players in the game world
-                $allPlayers = [];
+                $allPlayers["firstload"] = 0;
                 foreach ($this->clients as $player) {
                     // var_dump($player->posX);
                     // var_dump($player->posY);
@@ -70,6 +70,7 @@ class Socket implements MessageComponentInterface {
                         $allPlayers[$player->userinfoUsername] = "$player->userinfoCharacter-$player->posX-$player->posY";
                     }
                 }
+
                 // Send the client the data of all logged in users
                 $client->send(json_encode($allPlayers));
                 
@@ -83,27 +84,43 @@ class Socket implements MessageComponentInterface {
 
     public function onMessage(ConnectionInterface $client, $msg) {
 
-        echo "Client $client->resourceId said $msg\n";
-
         // Character moving
-        if(preg_match_all("/^\/move x(\d+) y(\d+) sprite(.+) flip(.+)$/", $msg, $matches)) {
+        if(preg_match_all("/^\/move x(.+) y(.+) t(.+)$/", $msg, $matches)) {
             $posX = $matches[1][0];
             $posY = $matches[2][0];
+            $time = $matches[3][0];
             // Save the player's updated location
             $client->posX = $posX;
             $client->posY = $posY;
-            // echo $client->posX." ".client->posY;
+            echo "$client->userinfoUsername moved to x$client->posX y$client->posY t$time\n";
 
-            // Broadcast new location to all other players
-            // foreach ($this->clients as $player) {
-            //     // if($client->userinfoUsername == $player->userinfoUsername) continue;
-            //     if($player->userinfoWorld == $client->userinfoWorld && $client->userinfoUsername !== $player->userinfoUsername) {
-            //         $player->send("[move] $client->userinfoUsername: x$player->posX y$player->posY");
-            //     }
-            // }
+            // Broadcast the update to all other players
+            foreach ($this->clients as $player) {
+                // if($client->userinfoUsername == $player->userinfoUsername) continue;
+                if($player->userinfoWorld == $client->userinfoWorld && $client->userinfoUsername !== $player->userinfoUsername) {
+                    $player->send("[move] $client->userinfoUsername: c$client->userinfoCharacter x$client->posX y$client->posY t$time");
+                }
+            }
+        }
+
+        // Update the client
+        if(preg_match("/^\/update$/", $msg)) {
+            $allPlayers = [];
+            foreach ($this->clients as $player) {
+                // if($client->userinfoUsername == $player->userinfoUsername) continue;
+                if($player->userinfoWorld == $client->userinfoWorld && $client->userinfoUsername !== $player->userinfoUsername) {
+                    $allPlayers[$player->userinfoUsername] = "$player->userinfoCharacter-$player->posX-$player->posY";
+                }
+            }
+
+            // Send the client the data of all logged in users
+            $client->send(json_encode($allPlayers));
+            return;
         }
 
         // Message handler for slash commands
+
+        echo "Client $client->resourceId said $msg\n";
 
         // /challenge <player_username>: create a new challenge record in db
         if(preg_match_all("/^\/challenge (.+)$/", $msg, $matches)) {
@@ -284,10 +301,13 @@ class Socket implements MessageComponentInterface {
     public function onClose(ConnectionInterface $client) {
         echo "Client $client->resourceId left\n";
 
+        // Disconnect the resource
+        $this->clients->detach($client);
+
         // Change the token in db to disconnected
         \Amp\Loop::run(function() use ($client) {
             // Get the auth token
-            parse_str($client->httpRequest->getUri()->getQuery(), $queryParameters);
+            // parse_str($client->httpRequest->getUri()->getQuery(), $queryParameters);
     
             require "../../../secrets.php";
             $config = \Amp\Mysql\ConnectionConfig::fromString(
