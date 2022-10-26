ef71d9b27e51000d037ce9bafd15c29eb32f4aa9
shaojingle <69612393+shaojingle@users.noreply.github.com>
2022-10-26 21:13:04 +0800

    LoginTest ResetPasswordTest

---
+++ tests/LoginTest.php
@@ -0,0 +1,63 @@
+<?php
+session_start();
+use PHPUnit\Framework\TestCase;
+
+class LoginTest extends TestCase{
+    public function testLoginSuccess(){
+        //login(string $uname, string $pass)
+        require('scripts\function_login.php');
+
+        // Insert student test case
+        $username = "studentacc";
+        $hash = password_hash("StudentPass123", PASSWORD_DEFAULT);
+        $email = "valid_email@email.com";
+        $name = "correct_name";
+        $sql = $conn->prepare("INSERT INTO `accounts`(`account_type`, `username`, `password`, `email`, `name`) VALUES (?, ?, ?, ?, ?)");
+        $sql->bind_param('sssss', "Student", $username, $hash, $email, $name);
+        $sql->execute();
+
+        $account_id = $conn->insert_id;
+        $students_insert = $conn->prepare("INSERT INTO `students`(`student_id`, `character_type`, `teacher_account_id`) VALUES (?, ?, ?)");
+        $students_insert->bind_param('iii', $account_id, 1, 1);
+        $students_insert->execute();
+
+        // Insert teacher test case
+        $username2 = "teacheracc";
+        $hash2 = password_hash("TeacherPass123", PASSWORD_DEFAULT);
+        $email2 = "valid_email2@email.com";
+        $name2 = "correct_name2";
+        $sql2 = $conn->prepare("INSERT INTO `accounts`(`account_type`, `username`, `password`, `email`, `name`) VALUES (?, ?, ?, ?, ?)");
+        $sql2->bind_param('sssss', "Teacher", $username2, $hash2, $email2, $name2);
+        $sql2->execute();
+
+        // Outputs: int 0 on success (student login)
+        $this -> assertEquals(0, login("studentacc", "StudentPass123"));
+        //          int 0 on success (teacher login)
+        $this -> assertEquals(0, login("teacheracc", "TeacherPass123"));
+        //          int 1 on incorrect password (student)
+        $this -> assertEquals(1, login("studentacc", "StudentPass12"));
+        //          int 1 on incorrect password (teacher)
+        $this -> assertEquals(1, login("teacheracc", "TeacherPass12"));
+        //          int 1 on incorrect username and password (student)
+        $this -> assertEquals(1, login("studentacc22", "StudentPass12"));
+        //          int 1 on incorrect username and password (teacher)
+        $this -> assertEquals(1, login("teacheracc22", "TeacherPass12"));
+        //          int 2 on server error after writing a value that exceeds what database can enter
+        $this -> assertEquals(2, login("999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999", "TeacherPass123"));
+
+        # Delete test cases 
+        $sql3 = $conn->prepare("DELETE FROM `accounts` WHERE username = ?");
+        $sql3->bind_param('s', $username);
+        $sql3->execute();
+
+        $sql4 = $conn->prepare("DELETE FROM `accounts` WHERE username = ?");
+        $sql4->bind_param('s', $username2);
+        $sql4->execute();
+
+        $sql5 = $conn->prepare("DELETE FROM `students` WHERE student_id = ?");
+        $sql5->bind_param('i', $account_id);
+        $sql5->execute();
+    }
+}
+
+
---
+++ tests/ResetPasswordTest.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+use PHPUnit\Framework\TestCase;
+
+class ResetPasswordTest extends TestCase{
+    public function testResetPasswordSuccess(){
+        //resetPassword(string $password, string $token)
+        require('scripts\function_resetPassword.php');
+
+        // Insert test cases
+        $username = "studentacc";
+        $hash = password_hash("validtoken", PASSWORD_DEFAULT);
+        $email = "valid_email@email.com";
+        $name = "correct_name";
+        $sql = $conn->prepare("INSERT INTO `accounts`(`account_type`, `username`, `password`, `email`, `name`) VALUES (?, ?, ?, ?, ?)");
+        $sql->bind_param('sssss', "Student", $username, $hash, $email, $name);
+        $sql->execute();
+
+        $account_id = $conn->insert_id;
+        $time = time();
+        $valid = 1;
+        $sql2 = $conn->prepare("INSERT INTO `password_resets` (`account_id`, `email_address`, `timestamp`, `hash`, `valid`) values (?, ?, ?, ?, ?)");
+        $sql2->bind_param("issii", $account_id, $email, $time, $hash, $valid);
+        $sql2->execute();
+
+        // Outputs: int 1 on invalid token
+        $this -> assertEquals(1, resetPassword("Cz3003password", "invalidtoken"));
+        //          int 2 on server error after writing a value that exceeds what database can enter
+        $this -> assertEquals(2, resetPassword("999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999", $hash));
+        //          int 3 on invalid password (length < 8)
+        $this -> assertEquals(3, resetPassword("Cz3003p", $hash));   
+        //          int 3 on invalid password (no numeric)
+        $this -> assertEquals(3, resetPassword("CzLoveSSAD", $hash));     
+        //          int 3 on invalid password (no lowercase)
+        $this -> assertEquals(3, resetPassword("CZLOVESSAD123", $hash));      
+        //          int 3 on invalid password (no uppercase)
+        $this -> assertEquals(3, resetPassword("czlovessad123", $hash)); 
+        //          int 0 on success
+        $this -> assertEquals(0, resetPassword("Cz3003password", $hash));
+
+        # Delete test cases 
+        $sql3 = $conn->prepare("DELETE FROM `accounts` WHERE username = ?");
+        $sql3->bind_param('s', $username);
+        $sql3->execute();
+
+        $sql4 = $conn->prepare("DELETE FROM `password_resets` WHERE account_id = ?");
+        $sql4->bind_param('i', $account_id);
+        $sql4->execute();
+    }
+}
+
+
