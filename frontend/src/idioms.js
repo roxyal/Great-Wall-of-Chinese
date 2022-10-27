d8c75476335f47e5c86018aee0cb38c3c3e31fb0
AsherFYD <AsherFYD@gmail.com>
2022-10-27 17:15:06 +0800

    test disabling keyboard input

--- frontend/src/idioms.js
+++ frontend/src/idioms.js
@@ -484,7 +484,7 @@ function update() {
     viewCustomLevelModal.addEventListener('show.bs.modal', function (event){
         console.log("HELLO FROM PHASER UPDATE FUNCTION")
         //this.cursors.enabled = false; //disable keyboard input
-        this.cursors.input.enabled = false;
+        this.input.keyboard.enabled = false;
     })
 
     // if (game.input.activePointer.withinGame){
