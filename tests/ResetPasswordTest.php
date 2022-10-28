b723211f5527049c68f604ee25d92c87c709e01b
shaojingle <CHAN0924@e.ntu.edu.sg>
2022-10-29 02:39:04 +0800

    updated test cases

--- tests/ResetPasswordTest.php
+++ tests/ResetPasswordTest.php
@@ -3,26 +3,26 @@ session_start();
 use PHPUnit\Framework\TestCase;
 
 class ResetPasswordTest extends TestCase{
-    public function testResetPasswordSuccess(){
+    public function ResetPasswordTest(){
         //resetPassword(string $password, string $token)
         require('scripts\function_resetPassword.php');
         require("scripts\config.php");
         require("scripts\functions_utility.php");
 
-        // Insert test cases
+        // Insert student test case
         $username = "studentacc";
         $hash = password_hash("validtoken", PASSWORD_DEFAULT);
         $email = "valid_email@email.com";
         $name = "correct_name";
-        $sql = $conn->prepare("INSERT INTO `accounts`(`account_type`, `username`, `password`, `email`, `name`) VALUES (?, ?, ?, ?, ?)");
-        $sql->bind_param('sssss', "Student", $username, $hash, $email, $name);
+        $sql = $conn->prepare("INSERT INTO `accounts`(`account_id`, `account_type`, `username`, `password`, `email`, `name`) VALUES (?, ?, ?, ?, ?, ?)");
+        $sql->bind_param('isssss', 9999, "Student", $username, $hash, $email, $name);
         $sql->execute();
 
-        $account_id = $conn->insert_id;
+        // Insert password reset test case
         $time = time();
         $valid = 1;
         $sql2 = $conn->prepare("INSERT INTO `password_resets` (`account_id`, `email_address`, `timestamp`, `hash`, `valid`) values (?, ?, ?, ?, ?)");
-        $sql2->bind_param("issii", $account_id, $email, $time, $hash, $valid);
+        $sql2->bind_param("issii", 9999, $email, $time, $hash, $valid);
         $sql2->execute();
 
         // Outputs: int 1 on invalid token
@@ -41,12 +41,12 @@ class ResetPasswordTest extends TestCase{
         $this -> assertEquals(0, resetPassword("Cz3003password", $hash));
 
         # Delete test cases 
-        $sql3 = $conn->prepare("DELETE FROM `accounts` WHERE username = ?");
-        $sql3->bind_param('s', $username);
+        $sql3 = $conn->prepare("DELETE FROM `accounts` WHERE account_id = ?");
+        $sql3->bind_param('i', 9999);
         $sql3->execute();
 
         $sql4 = $conn->prepare("DELETE FROM `password_resets` WHERE account_id = ?");
-        $sql4->bind_param('i', $account_id);
+        $sql4->bind_param('i', 9999);
         $sql4->execute();
     }
 }
--- tests/studentTest.php
+++ tests/studentTest.php
@@ -4,24 +4,22 @@
 
 class studentTest extends PHPUnit\Framework\TestCase{
     
-    public function testViewProfile(){
+    public function ViewProfileTest(){
 
         require("scripts\config.php");
         require("scripts\functions_utility.php");
-        require('scripts\function_createAccount.php');
         
         // Insert student test case
-        $account_id = 9999; 
         $sql = $conn->prepare("INSERT INTO `students`(`student_id`, `character_type`, `teacher_account_id`) VALUES (?, ?, ?)");
-        $sql->bind_param('iii', $account_id, 1, 1);
+        $sql->bind_param('iii', 9999, 1, 1);
         $sql->execute();
 
         $sql2 = $conn->prepare("INSERT INTO `accounts`(`account_id`, `account_type`, `username`, `password`, `email`, `name`) VALUES (?, ?, ?, ?, ?, ?)");
-        $sql2->bind_param('isssss', 9999, "Student", "studentacc", "StudentPass123", "valid_email@email.com", "correct_name");
+        $sql2->bind_param('isssss', 9999, "Student", "existinguser", "StudentPass123", "valid_email@email.com", "correct_name");
         $sql2->execute();
         
         $test_profile = array(
-                        'student_id' => $account_id,
+                        'student_id' => 9999,
                         'character_type'   => 1,
                         'teacher_account_id'  => 1,
                         'idiom_lower_correct' => 0,
@@ -43,91 +41,97 @@ class studentTest extends PHPUnit\Framework\TestCase{
                         'pinyin_upper_attempted'  => 0,
                         'pinyin_upper_reset_date'  => 0);
                 
-        // Outputs: int 0 on success on viewing the profile (return an Array)
-        $student_id = 9999;
-        $result_0 = $student->viewProfile($student_id);
-        $this->assertEquals($test_profile, $result_0);
+        // Outputs: int 0 on success
+        $this->assertEquals($test_profile, $student->viewProfile("existinguser"));
         
         //          int 1 on invalid account id
-        $student_id = 9998;
-        $result_1 = $student->viewProfile($student_id);
-        $this->assertEquals(0, $result_1);
+        $this->assertEquals(1, $student->viewProfile("nonexistinguser"));
 
         //          int 2 on server error
-        $student_id = 999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999;
-        $result_2 = $student->viewProfile($student_id);
-        $this->assertEquals(0, $result_2);
+        $this->assertEquals(2, $student->viewProfile("999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999"));
 
-        # Delete successful testcase after testing 
+        # Delete test cases 
         $sql3 = $conn->prepare("DELETE FROM `accounts` WHERE account_id = ?");
-        $sql3->bind_param('s', $account_id);
+        $sql3->bind_param('i', 9999);
         $sql3->execute();
 
         $sql4 = $conn->prepare("DELETE FROM `students` WHERE student_id = ?");
-        $sql4->bind_param('i', $account_id);
+        $sql4->bind_param('i', 9999);
         $sql4->execute();
     }
 
-    public function testSendPvpRequestTest(){
+    // public function SendPvpRequestTest(){
 
-        require 'scripts\config.php';
-        $student_conn = new Student($conn);
+    //     require('scripts\config.php');
+    //     require("scripts\functions_utility.php");
+    //     require('scripts\function_createAccount.php');
+
+    //     // Insert student test cases
+    //     $sql = $conn->prepare("INSERT INTO `students`(`student_id`, `character_type`, `teacher_account_id`) VALUES (?, ?, ?)");
+    //     $sql->bind_param('iii', 9999, 1, 1);
+    //     $sql->execute();
+
+    //     $sql2 = $conn->prepare("INSERT INTO `students`(`student_id`, `character_type`, `teacher_account_id`) VALUES (?, ?, ?)");
+    //     $sql2->bind_param('iii', 9998, 1, 1);
+    //     $sql2->execute();
+
+
         
-        // Outputs: int 0 on success on sending Pvp invitation to opponent
-        //          int 1 on user SendPvpRequest and choose CustomGame Mode, but user never create before 
-        //          int 2 on server error
+    //     // Outputs: int 0 on success on sending Pvp invitation to opponent
+    //     //          int 1 on user SendPvpRequest and choose CustomGame Mode, but user never create before 
+    //     //          int 2 on server error
 
-        // Return 1
-        $requester_id_noCustomGame = 1;
-        $opponent_id = 3;
-        $pvp_room_type = 0; // Choose CustomGame Mode
-        $result_1 = $student_conn->sendPvpRequest($requester_id_noCustomGame, $opponent_id, $pvp_room_type);
-        $this->assertEquals(1, $result_1);
+    //     // Return 1
+    //     $requester_id_noCustomGame = 1;
+    //     $opponent_id = 3;
+    //     $pvp_room_type = 0; // Choose CustomGame Mode
+    //     $result_1 = $student_conn->sendPvpRequest($requester_id_noCustomGame, $opponent_id, $pvp_room_type);
+    //     $this->assertEquals(1, $result_1);
 
-        // Return 0 
-        $requester_id_noCustomGame = 3;
-        $opponent_id = 1;
-        $pvp_room_type = 0; // Choose CustomGame Mode
-        $result_1 = $student_conn->sendPvpRequest($requester_id_noCustomGame, $opponent_id, $pvp_room_type);
-        $this->assertEquals(0, $result_1);
-    }
+    //     // Return 0 
+    //     $requester_id_noCustomGame = 3;
+    //     $opponent_id = 1;
+    //     $pvp_room_type = 0; // Choose CustomGame Mode
+    //     $result_1 = $student_conn->sendPvpRequest($requester_id_noCustomGame, $opponent_id, $pvp_room_type);
+    //     $this->assertEquals(0, $result_1);
+    // }
 
-    public function testAcceptPvpRequestTest(){
+    // public function AcceptPvpRequestTest(){
 
-        require 'scripts\config.php';
-        $student_conn = new Student($conn);
+    //     require 'scripts\config.php';
+    //     $student_conn = new Student($conn);
         
-        // Outputs: int 0 on success on accept/reject pvp request
-        //          int 2 on server error
+    //     // Outputs: int 0 on success on accept/reject pvp request
+    //     //          int 2 on server error
         
-        $requester_id = 1;
-        $opponent_id = 3;
-        $status = 0; 
-        $result_0 = $student_conn->acceptPvpRequest($requester_id, $opponent_id, $status);
-        $this->assertEquals(0, $result_0);
-    }
+    //     $requester_id = 1;
+    //     $opponent_id = 3;
+    //     $status = 0; 
+    //     $result_0 = $student_conn->acceptPvpRequest($requester_id, $opponent_id, $status);
+    //     $this->assertEquals(0, $result_0);
+    // }
 
-    public function testCreateCustomGameTest(){
+    // public function CreateCustomGameTest(){
 
-        require 'scripts\config.php';
-        $student_conn = new Student($conn);
+    //     require 'scripts\config.php';
+    //     $student_conn = new Student($conn);
         
-        // Outputs: int 0 on success on accept pvp request
-        //          int 2 on server error
+    //     // Outputs: int 0 on success on accept pvp request
+    //     //          int 2 on server error
         
-        $account_id = 4;
-        $idiom_lower_count = 2;
-        $idiom_upper_count = 0;
-        $fill_lower_count = 0;
-        $fill_upper_count = 3;
-        $pinyin_lower_count = 0;
-        $pinyin_upper_count = 0;
+    //     $account_id = 4;
+    //     $idiom_lower_count = 2;
+    //     $idiom_upper_count = 0;
+    //     $fill_lower_count = 0;
+    //     $fill_upper_count = 3;
+    //     $pinyin_lower_count = 0;
+    //     $pinyin_upper_count = 0;
 
-        $result_0 = $student_conn->createCustomGame($account_id, $idiom_lower_count, $idiom_upper_count,
-                                                    $fill_lower_count, $fill_upper_count,
-                                                    $pinyin_lower_count, $pinyin_upper_count);
-        $this->assertEquals(0, $result_0);
-    }
+    //     $result_0 = $student_conn->createCustomGame($account_id, $idiom_lower_count, $idiom_upper_count,
+    //                                                 $fill_lower_count, $fill_upper_count,
+    //                                                 $pinyin_lower_count, $pinyin_upper_count);
+    //     $this->assertEquals(0, $result_0);
+    // }
 }
 
 
