d390b4e1eaf49bea911f7d2bedf8a1a3d3337e72
shaojingle <CHAN0924@e.ntu.edu.sg>
2022-10-29 05:14:55 +0800

    added many tests

--- tests/CreateAccountTest.php
+++ tests/CreateAccountTest.php
@@ -9,40 +9,38 @@ class CreateAccountTest extends TestCase{
         require("scripts\config.php");
         require("scripts\functions_utility.php");
 
+        // Insert teacher test case
+        $sql = $conn->prepare("INSERT INTO `accounts`(`account_id`, `account_type`, `username`, `password`, `email`, `name`) VALUES (?, ?, ?, ?, ?, ?)");
+        $sql->bind_param('isssss', 1111, "Teacher", "teacheracc", "TeacherPass123", "valid_email0@email.com", "correct_name");
+        $sql->execute();
+
         // Outputs: int 0 on success
-        $this -> assertEquals(0, createAccount("correctusername", "correct_name", "valid_email@email.com", "Cz3003234a@sdfasdf", 1, 1));
+        $this -> assertEquals(0, createAccount("correctusername", "correct_name", "valid_email@email.com", "Cz3003password", 1111, 1));
         //          int 1 on email taken 
-        $this -> assertEquals(1, createAccount("correctusername", "correct_name", "taken_email@email.com", "Cz3003234a@sdfasdf", 1, 1));
+        $this -> assertEquals(1, createAccount("correctusername", "correct_name", "valid_email@email.com", "Cz3003password", 1111, 1));
         //          int 2 on username taken
-        $this -> assertEquals(2, createAccount("takenusername", "correct_name", "valid_email@gmail.com", "Cz3003234a@sdfasdf", 1, 1));
-        //          int 3 on invalid teacher
-        $this -> assertEquals(3, createAccount("correctusername", "correct_name", "valid_email@gmail.com", "Cz3003234a@sdfasdf", 98999999, 1));   
+        $this -> assertEquals(2, createAccount("correctusername", "correct_name", "valid_email@gmail.com", "Cz3003password", 1111, 1));
+        //          int 3 on invalid teacher id
+        $this -> assertEquals(3, createAccount("correctusername3", "correct_name", "valid_email3@gmail.com", "Cz3003password", 9999, 1));   
         //          int 4 on invalid character id
-        $this -> assertEquals(4, createAccount("correctusername", "correct_name", "valid_email@gmail.com", "Cz3003234a@sdfasdf", 1, 9999));   
+        $this -> assertEquals(4, createAccount("correctusername4", "correct_name", "valid_email4@gmail.com", "Cz3003password", 1111, 9999));   
         //          int 5 on server error after writing a value that exceeds what database can enter
-        $this -> assertEquals(5, createAccount("correctusername", "correct_name", "valid_email@gmail.com", "Cz3003234a@sdfasdf", 1, 999999999999999));   
+        $this -> assertEquals(5, createAccount("correctusername5", "correct_name", "valid_email5@gmail.com", "Cz3003password", 1111, 999999999999999));   
         //          int 6 on invalid email format
-        $this -> assertEquals(6, createAccount("correctusername", "correct_name", "invalid_email", "Cz3003234a@sdfasdf", 1, 1));   
+        $this -> assertEquals(6, createAccount("correctusername6", "correct_name", "invalid_email", "Cz3003password", 1111, 1));   
         //          int 7 on invalid username format
-        $this -> assertEquals(7, createAccount("a", "cool_name", "asdf@gmail.com", "valid_email@gmail.com", 1, 1));
+        $this -> assertEquals(7, createAccount("a", "correct_name", "valid_email7@gmail.com", "Cz3003password", 1111, 1));
         //          int 8 on invalid password format
-        $this -> assertEquals(8, createAccount("correctusername", "correct_name", "valid_email@gmail.com", "wrong_password", 1, 1));  
+        $this -> assertEquals(8, createAccount("correctusername8", "correct_name", "valid_email8@gmail.com", "wrongpassword", 1, 1));  
 
         # Delete test cases 
-        $sql = $conn->("SELECT `account_id` FROM `accounts` WHERE username = ?")
-        $sql->bind_param('s', "correctusername");
+        $sql = $conn->prepare("DELETE FROM `accounts` WHERE `name` = ?");
+        $sql->bind_param('s', "correct_name");
         $sql->execute();
-        $sql->store_result();
-        $sql->bind_result($account_id);
-        $sql->fetch();
 
-        $sql2 = $conn->prepare("DELETE FROM `accounts` WHERE username = ?");
-        $sql2->bind_param('s', "correctusername");
+        $sql2 = $conn->prepare("DELETE FROM `students` WHERE `teacher_account_id` = ?");
+        $sql2->bind_param('i', 1111);
         $sql2->execute();
-
-        $sql3 = $conn->prepare("DELETE FROM `students` WHERE student_id = ?");
-        $sql3->bind_param('i', $account_id);
-        $sql3->execute();
     }
 }
 
--- tests/teacherTest.php
+++ tests/teacherTest.php
@@ -3,39 +3,90 @@
 //use PHPUnit\Framework\TestCase;
 
 class teacherTest extends PHPUnit\Framework\TestCase{
-    public function testViewSummaryReportTest(){
-        require 'scripts\config.php';
-        require 'scripts\teacher.php';
 
-        // Outputs: int 0 on success
-        //          int 1 on no student tied with the teacher
-        //          int 2 on server error
+    public function CreateAssignmentTest(){
+        //createAssignment(string $assignment_name, int $account_id, int $created_timestamp, int $due_timestamp, string $questions)
+        require("scripts\config.php");
+        require("scripts\functions_utility.php");
+        require("scripts\teacher.php");
         
-        // Create a class teacher so that we call its function
-        $teacher = new Teacher($conn);
+        // Insert teacher test cases
+        $time = time();
+        $sql = $conn->prepare("INSERT INTO `accounts`(`account_id`, `account_type`, `username`, `password`, `email`, `name`) VALUES (?, ?, ?, ?, ?, ?)");
+        $sql->bind_param('isssss', 9999, "Teacher", "teacheracc", "TeacherPass123", "valid_email@email.com", "correct_name");
+        $sql->execute();
 
-        // Taking a teacher's account_id with no student tied to it
-        $noStudent_teacher_account_id = 3;
-        $result_1 = $teacher->viewSummaryReport($noStudent_teacher_account_id);
-        $this->assertEquals(1, $result_1);
+        // Outputs: int 0 on success (1 qn)
+        $this->assertEquals(0, $teacher->createAssignment("testassignment", 9999, $time, $time, "qn,ans1,ans2,ans3,ans4,ans,explain"));
+        //          int 0 on success (>1 qn)
+        $this->assertEquals(0, $teacher->createAssignment("testassignment2", 9999, $time, $time, "qn,ans1,ans2,ans3,ans4,ans,explain|qn,ans1,ans2,ans3,ans4,ans,explain"));
+        //          int 1 on non existing teacher
+        $this->assertEquals(1, $teacher->createAssignment("testassignment3", 1111, $time, $time, "qn,ans1,ans2,ans3,ans4,ans,explain"));
+        //          int 2 on existing assignment name
+        $this->assertEquals(2, $teacher->createAssignment("testassignment", 9999, $time, $time, "qn,ans1,ans2,ans3,ans4,ans,explain"));
+        //          int 3 on assignment name < length 2
+        $this->assertEquals(3, $teacher->createAssignment("a", 9999, $time, $time, "qn,ans1,ans2,ans3,ans4,ans,explain"));
+        //          int 4 on server error
+        $this->assertEquals(4, $teacher->createAssignment("999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999", 9999, $time, $time, "qn,ans1,ans2,ans3,ans4,ans,explain"));
 
-        // Lets say the teacher has one student tied under him
-        $test_array = array(0=>array(
-                            'student_id' => 1,
-                            'character_type'   => 'Wu Kong',
-                            'idiom_lower_accuracy' => 15,
-                            'idiom_upper_accuracy' => 29,
-                            'fill_lower_accuracy'  => 74,
-                            'fill_upper_accuracy'  => 23,
-                            'pinyin_lower_accuracy'=> 32,
-                            'pinyin_upper_accuracy'=> 60));
-        
-        $hasStudent_teacher_account_id = 2;
-        $result_0 = $teacher->viewSummaryReport($hasStudent_teacher_account_id);
-        $this->assertEquals($test_array, $result_0 );
+        # Delete test cases 
+        $sql2 = $conn->prepare("DELETE FROM `questions_bank` WHERE `account_id` = ?");
+        $sql2->bind_param('i', 9999);
+        $sql2->execute();
+
+        $sql3 = $conn->prepare("DELETE FROM `assignments` WHERE `account_id` = ?");
+        $sql3->bind_param('i', 9999);
+        $sql3->execute();
+
+        $sql4 = $conn->prepare("DELETE FROM `accounts` WHERE `account_id` = ?");
+        $sql4->bind_param('i', 9999);
+        $sql4->execute();
     }
 
+    public function ViewSummaryReportTest(){
+        require("scripts\config.php");
+        require("scripts\functions_utility.php");
+        require("scripts\teacher.php");
+        
+        // Insert student test case
+        $sql = $conn->prepare("INSERT INTO `accounts`(`account_id`, `account_type`, `username`, `password`, `email`, `name`) VALUES (?, ?, ?, ?, ?, ?)");
+        $sql->bind_param('isssss', 9997, "Student", "studentacc", "StudentPass123", "valid_email@email.com", "correct_name");
+        $sql->execute();
 
+        $sql2 = $conn->prepare("INSERT INTO `students`(`student_id`, `character_type`, `teacher_account_id`) VALUES (?, ?, ?)");
+        $sql2->bind_param('iii', 9997, 1, 9999);
+        $sql2->execute();
+
+        // Insert teacher test cases
+        $sql3 = $conn->prepare("INSERT INTO `accounts`(`account_id`, `account_type`, `username`, `password`, `email`, `name`) VALUES (?, ?, ?, ?, ?, ?)");
+        $sql3->bind_param('isssss', 9999, "Teacher", "teacheracc", "TeacherPass123", "valid_email2@email.com", "correct_name");
+        $sql3->execute();
+
+        $sql4 = $conn->prepare("INSERT INTO `accounts`(`account_id`, `account_type`, `username`, `password`, `email`, `name`) VALUES (?, ?, ?, ?, ?, ?)");
+        $sql4->bind_param('isssss', 9998, "Teacher", "teacheracc2", "TeacherPass123", "valid_email3@email.com", "correct_name");
+        $sql4->execute();
+
+        // Lets say the teacher has one student tied under him
+        $test_string = "correct_name,0,0,0,0,0,0,0,0,0,0,0,0";
+        
+        // Outputs: int 0 on success
+        $this->assertEquals($test_string, $teacher->viewSummaryReport(9999));
+        //          int 1 on non existing teacher
+        $this->assertEquals(1, $teacher->viewSummaryReport(1111));
+        //          int 2 on no students under the teacher
+        $this->assertEquals(2, $teacher->viewSummaryReport(9998));
+        //          int 3 on server error
+        $this->assertEquals(3, $teacher->viewSummaryReport(9999999999999999999999999999999999999999));
+
+        # Delete test cases 
+        $sql5 = $conn->prepare("DELETE FROM `accounts` WHERE `name` = ?");
+        $sql5->bind_param('s', "correct_name");
+        $sql5->execute();
+
+        $sql6 = $conn->prepare("DELETE FROM `students` WHERE student_id = ?");
+        $sql6->bind_param('i', 9997);
+        $sql6->execute();
+    }
 }
 
 
